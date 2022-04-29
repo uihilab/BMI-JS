d24310a0a9a59e79cddb319d5738207463e8d355
ashanivaidya <vaidyaishani@gmail.com>
2022-04-29 12:57:21 -0500

    test-cases

--- test/test-cases/bmiheat.test.js
+++ test/test-cases/bmiheat.test.js
@@ -1,1 +1,28 @@
-const bmiheat = require("../heat/bmiheat.js");
+const BmiHeat = require("../heat/bmiheat.js");
+
+let delta;
+let varName;
+let gridSize;
+let initialTemp;
+
+beforeAll(() => {
+  delta = 0.1;
+  varName = "plate_surface__temperature";
+  gridSize = 48;
+  initialTemp = 0.0;
+});
+
+let bmi = new BmiHeat();
+bmi.initialize("../data/heat.json");
+
+describe("get_value", () => {
+  let varCpy1 = new Array(gridSize);
+  let varCpy2 = new Array(gridSize);
+
+  let a1 = bmi.get_value(varName, varCpy1);
+  let a2 = bmi.get_value(varName, varCpy2);
+
+  test("test that they are not same", () => {
+    expect(a1).not.toBe(a2);
+  });
+});
